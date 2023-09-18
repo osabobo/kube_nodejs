3ffc1384f44e9a799223e38abf8c44257618cc4a
osagie eboigbe <36449465+osabobo@users.noreply.github.com>
2023-09-18 15:57:07 +0100

    Update

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 provider "kubernetes" {
   config_path = "~/.kube/config"
+  
 }
 
-resource "kubernetes_manifest" "node_app_deployment" {
-  yaml_body = file("${path.module}/deployment.yaml")
+resource "null_resource" "apply_k8s_manifest" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${path.module}/deployment.yaml"
+  }
 }
