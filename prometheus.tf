82d775da5272cdce7fa6877d8b6806c4012e15b9
osagie eboigbe <36449465+osabobo@users.noreply.github.com>
2023-09-21 23:19:09 +0100

    Update

--- prometheus.tf
+++ prometheus.tf
@@ -4,10 +4,20 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   chart      = "prometheus-community/kube-prometheus-stack"
   version    = "13.5.0"
-  namespace  = "monitoring"
+  namespace  = "default"
   wait       = true
+  timeout    = 15
+
+  set {
+    name  = "service.type"
+    value = "NodePort"
+  }
 }
